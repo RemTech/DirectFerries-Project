61b716538b3da46ebeec484edd96dab6c82aa538
remtech <remi.osisanya@remtech.dev>
2023-11-16 19:10:55 +0000

    fix(): modifications to the variables

--- src/methods/italian-language.js
+++ src/methods/italian-language.js
@@ -9,13 +9,15 @@ import { ClientFunction, Selector, t } from "testcafe";
 const specialOffers = 'Special offers';
 const resource = '/offers.htm';
 const italianPageText = 'Compagnie Navali';
-const top_Level_Domain= ClientFunction(() => window.location.href.toString());
-const pathName=ClientFunction(() => window.location.pathname.toString());
-const attrKey='href';
-const attrValue='/offers.htm';
-const topLevelDomain='.it';
-const baseURL='https://www.directferries.com';
-const element=Selector('div.user-actions');
+const top_Level_Domain = ClientFunction(() => window.location.href.toString());
+const pathName = ClientFunction(() => window.location.pathname.toString());
+const attrKey = 'href';
+const attrValue = '/offers.htm';
+const topLevelDomain = '.it';
+const baseURL = 'https://www.directferries.com';
+const element = Selector('div.user-actions');
+const anchorTag = Selector('a');
+const h3Tag = Selector('h3');
 
 /**
  * @function homepage
@@ -25,7 +27,7 @@ export const homePage = async () => {
         .maximizeWindow()
         .navigateTo(baseURL)
         .wait(2)
-        .hover(Selector('a').withExactText(specialOffers))
+        .hover(anchorTag.withExactText(specialOffers))
 };
 
 /**
@@ -34,7 +36,7 @@ export const homePage = async () => {
 export const clickSpecialOffers = async () => {
     await t
         .wait(3)
-        .click(Selector('a').withAttribute(attrKey,attrValue))
+        .click(anchorTag.withAttribute(attrKey, attrValue))
 };
 
 /**
@@ -52,22 +54,23 @@ export const special_Offers_Page = async () => {
  * @issue dimention for the property is 0 X 0 
  */
 export const countryIcon = async () => {
-    //div.df__navbar-actions > section > div > div > div > a 
-        await
-         t
-         .wait(2)
-         .takeElementScreenshot(element.find('a').withAttribute('aria-label','Language Dropdown').filterHidden())
-         .click(Selector(element,{visibilityCheck:true}).find('a').withAttribute('aria-label','Language Dropdown').filterHidden());
+
+    await
+        t
+            .wait(2)
+            .takeElementScreenshot(element.find('a').withAttribute('aria-label', 'Language Dropdown').filterHidden())
+            .click(Selector(element, { visibilityCheck: true }).find('a').withAttribute('aria-label', 'Language Dropdown').filterHidden());
 };
 
 /**
  * @function confirm page is italian with Top Level Domain Name(TLD) .it
  */
 export const italianPage = async () => {
-   
+
     await t
         .wait(3)
         .takeScreenshot()
-        .expect(Selector('h3').innerText).eql(italianPageText)
+        .expect(h3Tag.innerText).eql(italianPageText)
         .expect(top_Level_Domain()).contains(topLevelDomain);
 };
+
